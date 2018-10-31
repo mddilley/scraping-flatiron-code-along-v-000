210e6dc23890da1d5fbb14815cd61d80d6170933
Mike Dilley <mike.dilley@gmail.com>
2018-10-31 17:20:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,11 +17,11 @@ class Scraper
   end
 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    @doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def get_courses
-    doc.css("#post")
+    @doc.css("#post")
   end
 
 end
